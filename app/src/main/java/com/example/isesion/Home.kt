7660ca2d4f122ca70ba83f01e589e466d309c590
Oscar-SH <oscrsnts29@gmail.com>
2022-03-05 00:30:17 -0600

    SEGUNDOj

--- app/src/main/java/com/example/isesion/Home.kt
+++ app/src/main/java/com/example/isesion/Home.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        Toast.makeText(this, "Usted esta disponible", Toast.LENGTH_SHORT).show()
     }
 
     fun irDe(view: View){
--- app/src/main/java/com/example/isesion/sesion.kt
+++ app/src/main/java/com/example/isesion/sesion.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 
 class sesion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sesion)
+        Toast.makeText(this, "Usted no esta disponible", Toast.LENGTH_SHORT).show()
     }
 
     fun irHome(view: View){
