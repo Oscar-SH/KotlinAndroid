bd39bc4c77f2e767e63a8e205568680238712722
Oscar-SH <oscrsnts29@gmail.com>
2022-03-06 23:41:56 -0600

    Listo

--- app/src/main/java/com/example/isesion/AcercaDe.kt
+++ app/src/main/java/com/example/isesion/AcercaDe.kt
@@ -1,11 +1,19 @@
 package com.example.isesion
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class AcercaDe : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_acerca_de)
     }
+    fun irGit(view: View){
+        val irG = Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/"))
+        startActivity(irG)
+    }
+
 }
